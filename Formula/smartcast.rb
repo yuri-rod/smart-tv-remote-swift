0ce50ae538610ca30680b2c025e926d99bc2fbf5
yuri-rod <yuri.rbarreira@gmail.com>
2026-08-29 23:16:47 -0300

    formula: apontar pra v1.1.1 e adicionar o sha256 que faltava
    sem sha256 o brew install recusa a formula antes mesmo de baixar.

--- Formula/smartcast.rb
+++ Formula/smartcast.rb
@@ -1,7 +1,8 @@
 class Smartcast < Formula
   desc "Control Smart TVs and cast media from your terminal"
   homepage "https://github.com/yuri-rod/smart-tv-remote-swift"
-  url "https://github.com/yuri-rod/smart-tv-remote-swift/archive/refs/tags/v1.1.0.tar.gz"
+  url "https://github.com/yuri-rod/smart-tv-remote-swift/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "8aa0f94f8aecb28ba11a69da21759e1206a9e9f41edc58cb3e788391c6044955"
   license "MIT"
 
   depends_on xcode: ["15.0", :build]
